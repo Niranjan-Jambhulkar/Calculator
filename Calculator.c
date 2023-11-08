15e40013a79d93de3d76966df15fa6737860b718
Niranjan <niranjan.jambhulkar@outlook.com>
2023-11-08 21:12:57 +0530

    Initial commit

---
+++ Calculator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "D:\Programs\C Project\Calculator\Functions\Function.c"
+int main(int argc, char const *argv[])
+{
+    Addition();
+    return 0;
+}
---
+++ Functions/Function.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void Addition();
+void Addition(){
+    int a=2,b=2,c;
+    c=a+b;
+    printf("%d", c);
+}
