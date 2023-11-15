51d78e089df4555a2934cfc41a51e54daa99b3d9
Niranjan <niranjan.jambhulkar@outlook.com>
2023-11-15 14:21:24 +0530

    Update

--- Functions/Function.c
+++ Functions/Function.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 void Addition();
 void Addition(){
-    int a;
+    int sum=0,temp;
+    int Add[20];
+    for (int i = 0; i < 20; i++)
+    {
+        printf("Enter Number: ");
+        scanf("%d", &Add[i]);
+        if (Add[i] == 0)
+        {
+            temp=i;
+            break;
+        }
+        
+    }
+    for (int i = 0; i < temp; i++)
+    {
+        sum = sum + Add[i];
+    }
+    printf("Sum is: %d", sum);
+    
 }
